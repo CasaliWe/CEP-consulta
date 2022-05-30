d92f1c1b4c5ce6551ad2e80506b46778883958a8
Weslei Casali <wesleicasali18@gmail.com>
2022-05-30 15:32:16 -0300

    projetoCEP

---
+++ cep.js
@@ -0,0 +1,55 @@
+async function consultarDados(){
+    const cep = document.getElementById('cep').value
+    const url = `http://viacep.com.br/ws/${cep}/json/`
+    const dados = await fetch(url)
+    const endereco = await dados.json()
+    respostaDados(endereco)
+}
+
+
+function respostaDados(endereco){
+
+    setTimeout(() => {
+   document.getElementById('rua').textContent = `Rua: ${endereco.logradouro};`
+   document.getElementById('rua').style.cssText = `display: block;`
+
+    }, 600);
+
+    setTimeout(() => {
+        document.getElementById('bairro').textContent = `Bairro: ${endereco.bairro};`
+        document.getElementById('bairro').style.cssText = `display: block;`
+     
+    }, 1200);
+
+    setTimeout(() => {
+        document.getElementById('cidade').textContent = `Cidade: ${endereco.localidade};`
+        document.getElementById('cidade').style.cssText = `display: block;`
+
+    }, 1800);
+
+    setTimeout(() => {
+        document.getElementById('estado').textContent = `Estado: ${endereco.uf};`
+        document.getElementById('estado').style.cssText = `display: block;`
+    }, 2400);
+
+    setTimeout(() => {
+        document.getElementById('nova-pesquisa').style.cssText = `display: block;`
+    }, 3000);
+   
+   
+}
+
+function reiniciar(){
+      window.location.href = "index.html"
+}
+
+var main = document.querySelector('main')
+
+function fadee(){
+    setTimeout(() => {
+        main.className = "fadeout"
+    }, 1300);
+}
+
+main.addEventListener('load', fadee())
+
